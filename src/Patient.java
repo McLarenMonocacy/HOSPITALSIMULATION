4a56f553af99902f04a19dc5b42ca50e015d90e3
McLarenMonocacy <w1313746@myfcc.frederick.edu>
2026-02-26 12:15:43 -0500

    WIP

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,16 @@
+import java.util.Date;
+import java.util.UUID;
+
 public class Patient {
+    private UUID uuid;
+    private String name;
+    private Date dateOfBirth;
+
+    public Patient(){
+
+    }
+
+    private static UUID generateUUID(){
+        return UUID.randomUUID();
+    }
 }
