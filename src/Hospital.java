03f8aeefb9ba494e12efa75ea9f9ed55c10618fa
mclarenmonocacy <w1313746@myfcc.frederick.edu>
2026-03-05 12:08:50 -0500

    Rough implementation of a linked list in the hospital class to hold patients

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,2 +1,23 @@
 public class Hospital {
+    private class LinkedPatient{
+        Patient heldPatient;
+        LinkedPatient nextLink = null;
+
+        LinkedPatient(Patient patient){
+            this.heldPatient = patient;
+        }
+
+        void add(Patient patient){
+            if (nextLink == null) nextLink = new LinkedPatient(patient);
+            else nextLink.add(patient);
+        }
+    }
+
+    LinkedPatient patientList;
+
+    public void addPatient(Patient patient){
+        if (patient == null) return;
+        if (patientList == null) patientList = new LinkedPatient(patient);
+        else patientList.add(patient);
+    }
 }
