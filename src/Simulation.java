5634d5215a249d2392578eaf4bfa9b2922574f6c
mclarenmonocacy <w1313746@myfcc.frederick.edu>
2026-03-05 11:56:53 -0500

    Start of class, doesn't do anything yet

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,2 +1,21 @@
 public class Simulation {
+    private boolean hasSimulationRun = false;
+    public Simulation(){
+        setup();
+    }
+
+    private void setup(){
+    }
+
+    public void run(){
+    }
+
+    public void process(){
+        if (!hasSimulationRun) return; //Can't process what hasn't happened
+    }
+
+    public static double random(){
+        //NOTE: needs setup to accept a seed on setup
+        return Math.random();
+    }
 }
