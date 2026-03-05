71f298b12a575387ef809a3f4081c507e55f6c2f
mclarenmonocacy <w1313746@myfcc.frederick.edu>
2026-03-05 12:13:05 -0500

    Added code in main that runs the simulation (once the simulation run() does things)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static Simulation sim = new Simulation();
+    static Simulation sim;
 
     public Simulation getSim() {
         return sim;
     }
 
     public static void main(String[] args) {
-
+        sim = new Simulation();
+        sim.run();
+        sim.process();
     }
 }
