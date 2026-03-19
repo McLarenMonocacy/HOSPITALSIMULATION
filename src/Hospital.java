1591630f87e40ef646e0eb64c04468cfd502003f
mclarenmonocacy <w1313746@myfcc.frederick.edu>
2026-03-19 11:28:48 -0400

    replaced the internal class linkedpatiented with the cacylinkedlist

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,34 +1,12 @@
 public class Hospital {
-    private class LinkedPatient{
-        //This will be replaced
-        Patient heldPatient;
-        LinkedPatient nextLink = null;
 
-        LinkedPatient(Patient patient){
-            this.heldPatient = patient;
-        }
-
-        void add(Patient patient){
-            if (nextLink == null) nextLink = new LinkedPatient(patient);
-            else nextLink.add(patient);
-        }
-    }
-
-    LinkedPatient patientList;
+    CacyLinkedList<Patient> patientList;
 
     public void addPatient(Patient patient){
-        if (patient == null) return;
-        if (patientList == null) patientList = new LinkedPatient(patient);
-        else patientList.add(patient);
+        patientList.add(patient);
     }
 
     public int patientCount(){
-        LinkedPatient link = patientList;
-        int count = 0;
-        while (link != null){
-            count++;
-            link = link.nextLink;
-        }
-        return count;
+        return patientList.length();
     }
 }
