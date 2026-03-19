94413f60639f1f6885e686fea27a997f2ae71b1b
dudewithajetpack <grantbugg101@gmail.com>
2026-03-19 11:59:08 -0400

    Created Alert class and constructor

---
+++ src/Alert.java
@@ -0,0 +1,15 @@
+public class Alert{
+    private double startTime;
+    private double endTime;
+    private Observation data;
+    private boolean urgent;
+
+    public Alert(double start, Observation obs){
+        startTime = start;
+        data = obs;
+        if (obs.veryDangerous()){
+            urgent = true;
+        }
+        else urgent = false;
+    }
+}
