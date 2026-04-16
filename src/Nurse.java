a501b12eb3ab07125ae329c2b4cfd1bd6f6d390a
McLarenMonocacy <w1313746@myfcc.frederick.edu>
2026-04-16 01:38:08 -0400

    Nurses have IDs

--- src/Nurse.java
+++ src/Nurse.java
@@ -1,12 +1,19 @@
+import java.util.UUID;
+
 public class Nurse {
 
+    private final UUID ID;
     private final double skillLevel;
 
     public Nurse (double skillLevel){
         this.skillLevel = skillLevel;
+        this.ID = UUID.randomUUID();
     }
 
     public double getSkillLevel() {
         return skillLevel;
+    }
+    public UUID getID() {
+        return ID;
     }
 }
--- test/NurseTest.java
+++ test/NurseTest.java
@@ -8,4 +8,11 @@ class NurseTest {
     void getSkillLevel() {
         assertEquals(2d,new Nurse(2).getSkillLevel());
     }
+    @Test
+    void UUID(){
+        Nurse nurse1 = new Nurse(1);
+        Nurse nurse2 = new Nurse(1);
+        assertNotNull(nurse1.getID(), "UUID is null");
+        assertNotEquals(nurse1.getID(),nurse2.getID(), "UUID are the same");
+    }
 }
